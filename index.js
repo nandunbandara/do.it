6184a10de42a9a5035b702c00067324c9404da85
Nandun Bandara <ntbandara3@hotmail.com>
2017-12-18 19:49:12 +0530

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const PORT = process.env.PORT || 7001;
+
+mongoose.connect('mongodb://root:pass123@ds161026.mlab.com:61026/alexa-api-doit', function(){
+  console.log('connected to database!');
+}, function(err){
+  throw new Error(err);
+})
+
+app
+  .post('/api/todos', addToDo)
+  .listen(PORT, function(err){
+    if(err){
+      console.log('Can not listen on PORT: ', PORT);
+      return;
+    }
+
+    console.log('Listening on PORT: ', PORT);
+  })
+
+let todoSchema = mongoose.Schema({
+  user: { type:String },
+  todo: { type:String }
+})
+
+let ToDo = mongoose.model('Todo', todoSchema);
+
+function addToDo(req,res){
+      console.log(req.body);
+}
